329425652ffcd75569521aa31d44089813cf7948
Saptorshi Majumder <52661102+SamRyder27@users.noreply.github.com>
2025-04-28 15:21:34 +0530

    query fix
    query fix in progress 2

--- src/main/java/com/samryder/employee_service/Repository/EmployeeRepository.java
+++ src/main/java/com/samryder/employee_service/Repository/EmployeeRepository.java
@@ -7,11 +7,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> extends CurdRepository<EmployeeEntity, Long>
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> , CrudRepository<EmployeeEntity, Long>
 {
 	
 //	@Query(value = "SELECT c FROM employeedb c WHERE c.name LIKE '%'||:keyword || '%'"
