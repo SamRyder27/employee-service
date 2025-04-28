3d2c9f915a58a4fe7fc511eb94e922235424c4e2
Saptorshi Majumder <52661102+SamRyder27@users.noreply.github.com>
2025-04-28 14:02:31 +0530

    query fix
    SQL query fixing in progress 1

--- src/main/java/com/samryder/employee_service/Repository/EmployeeRepository.java
+++ src/main/java/com/samryder/employee_service/Repository/EmployeeRepository.java
@@ -11,11 +11,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long>{
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> extends CurdRepository<EmployeeEntity, Long>
+{
 	
-	@Query(value = "SELECT c FROM Employee c WHERE c.name LIKE '%'||:keyword || '%'"
-            + " OR c.email LIKE '%' || :keyword || '%'")
+//	@Query(value = "SELECT c FROM employeedb c WHERE c.name LIKE '%'||:keyword || '%'"
+//            + " OR c.email LIKE '%' || :keyword || '%'")
             //+ " OR c.address LIKE '%' || :keyword || '%'") 
+	
+	@Query (value = "SELECT e FROM EmployeeDB e WHERE e.name LIKE %:keyword% AND e.email LIKE %:keyword%")
 	List<Employee> search (@Param("keyword") String keyword);
 	
 }
